dfe40e068afd189b983982000c4879bea856692b
Carl Goldsmith <me@carlgoldsmith.com>
2020-05-03 15:50:52 +0100

    Updates

---
+++ precache-manifest.602253f071e284b6ea03382506b46f8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335d01ea69cd43cb1f74b32c1546f28e",
+    "url": "/AutoMQG/index.html"
+  },
+  {
+    "revision": "a262d945860b5919d292",
+    "url": "/AutoMQG/static/css/2.85ec7da2.chunk.css"
+  },
+  {
+    "revision": "4b1ed082bbcd98b420da",
+    "url": "/AutoMQG/static/css/main.4e2ae438.chunk.css"
+  },
+  {
+    "revision": "a262d945860b5919d292",
+    "url": "/AutoMQG/static/js/2.4fb2fc86.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/AutoMQG/static/js/2.4fb2fc86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b1ed082bbcd98b420da",
+    "url": "/AutoMQG/static/js/main.94cb0a48.chunk.js"
+  },
+  {
+    "revision": "fe79dc12ac2c15a9a65e",
+    "url": "/AutoMQG/static/js/runtime-main.b3c4e052.js"
+  }
+]);
