a3af520fe46794543f0be835f494d81afcf12fe8
Carl Goldsmith <me@carlgoldsmith.com>
2020-05-10 13:06:01 +0100

    Started API

---
+++ api/server.js
@@ -0,0 +1,40 @@
+// Load environment variables
+require('dotenv').config();
+
+// Set up the main Express app
+const app = require('express')();
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+
+// Handle cors
+var cors = require('cors');
+
+// Get the port from the environment
+const port = process.env.API_PORT || 5050;
+
+mongoose.connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Cors
+const whitelist = ['*'];
+app.use(cors({
+    origin: function(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            // callback(new Error('Not allowed by CORS'));
+            callback(null, true);
+        }
+    }
+}));
+
+// Set up routing
+app.use(bodyParser.json());
+app.use('/', routes);
+
+// Make the server listen
+app.listen(port, () => {
+    console.log(`App runnng on port ${port}`);
+})
