ada4c017a01cd872486f6f4adcb4bacca16d56b9
Carl Goldsmith <carl.goldsmith@me.com>
2020-06-02 08:39:55 +0100

    Cleaned up semicolons for consistency

--- api/server.js
+++ api/server.js
@@ -1,35 +1,35 @@
 // Load environment variables
-require('dotenv').config();
+require('dotenv').config()
 
 // Set up the main Express app
-const app = require('express')();
-const routes = require('./routes');
-const bodyParser = require('body-parser');
+const app = require('express')()
+const routes = require('./routes')
+const bodyParser = require('body-parser')
 
 // Handle cors
-var cors = require('cors');
+var cors = require('cors')
 
 // Get the port from the environment
-const port = process.env.API_PORT || 5050;
+const port = process.env.API_PORT || 5050
 
 // Cors
-const whitelist = ['*'];
+const whitelist = ['*']
 app.use(cors({
     origin: function(origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
+            callback(null, true)
         } else {
             // callback(new Error('Not allowed by CORS'));
-            callback(null, true);
+            callback(null, true)
         }
     }
 }));
 
 // Set up routing
-app.use(bodyParser.json());
-app.use('/', routes);
+app.use(bodyParser.json())
+app.use('/', routes)
 
 // Make the server listen
 app.listen(port, () => {
-    console.log(`App runnng on port ${port}`);
+    console.log(`App runnng on port ${port}`)
 })
