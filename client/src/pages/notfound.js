dc62605124c6ee62d7ce6ee2c7217def6996af59
System Administrator <root@remotemac.cable.virginmedia.net>
2020-06-06 21:44:23 +0100

    Updated 404 page

--- client/src/pages/notfound.js
+++ client/src/pages/notfound.js
@@ -1,26 +1,38 @@
 // Get the main things we need
-import React, { Component } from 'react';
-import { Jumbotron, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Jumbotron, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+// Get the error messages
+import messages from '../404Messages.json'
 
 // TODO: Add a random equation in here, just for fun
 
 export default class NotFound extends Component {
 
 
-    render () {
-        const errString = "{ Witty 404 Message Goes Here }"
+    render() {
+        const randomProblem = messages[Math.floor(Math.random() * messages.length)]
 
         return (
-            <div style={{ marginTop: "20px"}}>
-                <Jumbotron>
-                    <h1>{ errString }</h1>
-                    <Jumbotron style={{ backgroundColor: "#AAA", marginTop: "40px"}}>
-                    <h4>This is where a maths question will go. Hilarious.</h4>
-                    </Jumbotron>
-                    <Button as={Link} to="/" className="float-right">Take Me Home</Button>
+            <>
+                <h1>Page Not Found</h1>
+                <Jumbotron style={{
+                    backgroundColor: "#FFF",
+                    fontFamily: "Times New Roman, Sans Serif",
+                    paddingTop: "40px",
+                    paddingRight: "5px",
+                    paddingLeft: "5px",
+                    paddingBottom: "60px"
+                }}>
+                    <h4><b>Here's a question instead:</b></h4>
+                    <h4>{Math.floor(Math.random() * 99)}) {randomProblem.text}</h4>
+                    <p className="float-left" style={{ marginTop: "20px" }}><a target="_blank" rel="noopener noreferrer" href={randomProblem.link.url}>{randomProblem.link.title}</a></p>
                 </Jumbotron>
-            </div>
+                <div style={{ width: "100%", marginTop: "40px", marginBottom: "20px" }} className="text-center">
+                    <Button as={Link} to="/">I can't solve that - take me home</Button>
+                </div>
+            </>
         )
     }
 }
