f652e172c809de44fb72dfe9f330a7de5836a920
Carl Goldsmith <carl.goldsmith@me.com>
2020-06-06 22:03:51 +0100

    Adjusted alignment of maths question

--- client/src/pages/notfound.js
+++ client/src/pages/notfound.js
@@ -1,6 +1,6 @@
 // Get the main things we need
 import React, { Component } from 'react'
-import { Jumbotron, Button } from 'react-bootstrap'
+import { Jumbotron, Button, Col, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 // Get the error messages
@@ -21,13 +21,20 @@ export default class NotFound extends Component {
                     backgroundColor: "#FFF",
                     fontFamily: "Times New Roman, Sans Serif",
                     paddingTop: "40px",
-                    paddingRight: "5px",
-                    paddingLeft: "5px",
+                    paddingRight: "1em",
+                    paddingLeft: "1em",
                     paddingBottom: "60px"
                 }}>
-                    <h4><b>Here's a question instead:</b></h4>
-                    <h4>{Math.floor(Math.random() * 99)}) {randomProblem.text}</h4>
-                    <p className="float-left" style={{ marginTop: "20px" }}><a target="_blank" rel="noopener noreferrer" href={randomProblem.link.url}>{randomProblem.link.title}</a></p>
+                    <Row>
+                        <h4><b>Here's a question instead:</b></h4>
+                    </Row>
+                    <Row>
+                        <Col sm={1} xs={2}><h4 style={{ textAlign: "right" }}>{Math.floor(Math.random() * 99)}) </h4></Col>
+                        <Col sm={11} xs={10}><h4>{randomProblem.text}</h4></Col>
+                    </Row>
+                    <Row>
+                        <p className="float-left" style={{ marginTop: "20px" }}><a target="_blank" rel="noopener noreferrer" href={randomProblem.link.url}>{randomProblem.link.title}</a></p>
+                    </Row>
                 </Jumbotron>
                 <div style={{ width: "100%", marginTop: "40px", marginBottom: "20px" }} className="text-center">
                     <Button as={Link} to="/">I can't solve that - take me home</Button>
